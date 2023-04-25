e8308c182fe37ce99bb729b827d885b1875e146a
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-04-25 15:09:48 +0700

    update edit code

--- config/viewEngine.js
+++ config/viewEngine.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const session = require('express-session');
 const flash = require('express-flash');
-const path = require('path');
+//const path = require('path');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 //config : cấu hình
 let configViewEngine = (app,SESSION_SECRET,SESSION_ALGORITHM) =>{
 
-    app.use(express.static(path.join(__dirname, "../public")));
-    app.set('views', path.join(__dirname, "../views"));
+    // app.use(express.static(path.join(__dirname, "../public")));
+    // app.set('views', path.join(__dirname, "../views"));
     app.use(session({
         secret: SESSION_SECRET, // Khóa bí mật để mã hóa session
         algorithm: SESSION_ALGORITHM, // Thuật toán mã hóa
@@ -16,14 +16,14 @@ let configViewEngine = (app,SESSION_SECRET,SESSION_ALGORITHM) =>{
         saveUninitialized: true
       }));
     app.use(flash());
-    app.use(express.static("./src/public"));
+    app.use(express.static("./public"));
     app.use(bodyParser.json());
     app.use(express.urlencoded({extended: true})); 
     app.use(express.json());    
     app.engine('cl7', handlebars.engine({extname : '.cl7',}));
     app.set('view engine','cl7');
-    // app.use(express.static("./src/views"));
-    // app.set("views", "./src/views");
+    app.use(express.static("./views"));
+    app.set("views", "./views");
 }
 
 module.exports = {
